bfbe0c8d52ab42540182df03cd44ffcb41041855
Drif <yanis.drif@efrei.net>
2026-04-10 15:46:45 +0200

    Modification pour mise en ligne

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express"); // sert a creer un serveur facilement et gere plus facilement les routes et les requetes http
 const mongoose = require("mongoose"); // pour se connecter a une base de donnÃ©es mongoDB et faire des requetes plus facilement
 const cors = require("cors");
+require("dotenv").config(); // pour charger les variables d'environnement depuis un fichier .env
 const miscRoutes = require("./routes/miscRoutes");
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -8,10 +9,10 @@ const roleRoutes = require("./routes/roleRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express(); // creer un serveur express
-app.use(express.json()); // pour pouvoir recevoir des donnÃ©es au format json dans les requetes http
-app.use(cors()); // pour autoriser les requetes provenant du  frontend qui tourne sur un autre port
+app.use(express.json()); // pour pouvoir recevoir des données au format json dans les requetes http
+app.use(cors()); 
 
-mongoose.connect("mongodb://localhost:27017/stock-api")
+mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
         console.log("Connected to MongoDB");
     })
@@ -25,6 +26,6 @@ app.use(productRoutes);
 app.use(roleRoutes);
 app.use(categoryRoutes);
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Server is running");
 });
